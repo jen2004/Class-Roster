7f217be92c28122a29343b2e4fd8ca912b5fe00c
Jessica <jnic331@wgu.edu>
2024-12-30 17:07:06 -0600

    update name for Jessica Short and create new screenshot of output

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main()
     cout << "Course Title: C867 - SCRIPTING AND PROGRAMMING - APPLICATIONS" << std::endl;
     cout << "Programming Language Used: C++ " << std::endl;
     cout << "Student ID: 010500487" << std::endl;
-    cout << "Student Name: Jessica Nichols" << std::endl;
+    cout << "Student Name: Jessica Short" << std::endl;
     cout << "   " << std::endl;
     cout << "   " << std::endl;
 
@@ -20,7 +20,7 @@ int main()
         "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
         "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
         "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
-        "A5,Jessica,Nichols,jnic331@wgu.edu,37,40,45,35,SOFTWARE"
+        "A5,Jessica,Short,jnic331@wgu.edu,37,40,45,35,SOFTWARE"
     };
 
 
